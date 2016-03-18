d777a5579f90c683a31a8f01d451f80c58ed0be6
xin.jin <xin.jin@baozun.com>
2016-03-18 17:22:57 +0800

    update javadoc

--- nebula-helix/src/main/java/com/baozun/nebula/web/bind/LoginMember.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/bind/LoginMember.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2008 feilong
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.baozun.nebula.web.bind;
 
 import java.lang.annotation.Documented;
@@ -22,7 +7,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 专门为了定制 参数指定的 ,参见 {@link LoginMemberHandlerMethodArgumentResolver}
+ * 标识spring mvc controller方法参数支持自动注入 用户session信息 ,参见 {@link LoginMemberHandlerMethodArgumentResolver}
  *
  * @author feilong
  * @version 1.5.0 2016年2月26日 下午7:36:41
@@ -30,9 +15,14 @@ import java.lang.annotation.Target;
  * 
  * @see LoginMemberHandlerMethodArgumentResolver
  */
+//表示产生文档,比如通过javadoc产生文档, 将此注解包含在 javadoc 中, 这个Annotation可以被写入javadoc
+//在默认情况下，注释 不包括在 Javadoc 中
 @Documented
-@Target({ ElementType.PARAMETER })
+//在jvm加载class时候有效, VM将在运行期也保留注释,因此可以通过反射机制读取注解的信息
 @Retention(RetentionPolicy.RUNTIME)
+
+//仅用于 Method parameter
+@Target({ ElementType.PARAMETER })
 public @interface LoginMember{
 
 }
--- nebula-helix/src/main/java/com/baozun/nebula/web/bind/LoginMemberHandlerMethodArgumentResolver.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/bind/LoginMemberHandlerMethodArgumentResolver.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2008 feilong
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.baozun.nebula.web.bind;
 
 import java.util.Map;
@@ -75,6 +60,7 @@ import com.feilong.core.util.Validator;
  * @version 1.5.0 2016年2月26日 下午6:49:44
  * @see org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter#getDefaultArgumentResolvers()
  * @since 1.5.0
+ * @since spring 3.1
  */
 public class LoginMemberHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver{
 
