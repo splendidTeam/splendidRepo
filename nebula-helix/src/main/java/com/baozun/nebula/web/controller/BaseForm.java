f7c12291c7c8aa31b808c36fc37118f95b5e0bfb
D.C <dianchao.song@baozun.cn>
2016-03-20 10:12:39 +0800

    merge

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/BaseForm.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/BaseForm.java
@@ -1,15 +1,15 @@
 /**
- * Copyright (c) 2016 Baozun All Rights Reserved.
+ * Copyright (c) 2010 Jumbomart All Rights Reserved.
  *
- * This software is the confidential and proprietary information of Baozun.
+ * This software is the confidential and proprietary information of Jumbomart.
  * You shall not disclose such Confidential Information and shall use it only in
  * accordance with the terms of the license agreement you entered into
- * with Baozun.
+ * with Jumbo.
  *
- * BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * JUMBOMART MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
  * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * PURPOSE, OR NON-INFRINGEMENT. JUMBOMART SHALL NOT BE LIABLE FOR ANY DAMAGES
  * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
  * THIS SOFTWARE OR ITS DERIVATIVES.
  *
@@ -18,12 +18,11 @@ package com.baozun.nebula.web.controller;
 
 import java.io.Serializable;
 
-/**
- * @author D.C
- * 2016年3月18日 下午5:26:47
- */
-public class BaseForm implements Serializable {
+public class BaseForm implements Serializable{
 
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 3718461871193121470L;
 
 }
