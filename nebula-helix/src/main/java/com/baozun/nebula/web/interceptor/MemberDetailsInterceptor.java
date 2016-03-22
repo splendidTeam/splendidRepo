fa24ab7b39465ab8ccc1f9caac44bc29ce8d14d2
Benjamin <benjamin.liu@baozun.com>
2016-03-22 21:38:25 +0800

    删除POM中重复或无价值的部分。因为Cookie的处理将使用金鑫的包，因此Tomcat的实现类也不再依赖 删除无用的validate内容 新增MemberDetailsInterceptor，用于控制登录环节，同时基于此增加了两个处理器和若干参数配置。具体参数配置可见HelixConstants

---
+++ nebula-helix/src/main/java/com/baozun/nebula/web/interceptor/MemberDetailsInterceptor.java
@@ -0,0 +1,192 @@
+/**
+ * Copyright (c) 2010 Jumbomart All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of Jumbomart.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jumbo.
+ *
+ * JUMBOMART MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. JUMBOMART SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ *
+ */
+package com.baozun.nebula.web.interceptor;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.UrlPathHelper;
+
+import com.baozun.nebula.web.HelixConfig;
+import com.baozun.nebula.web.HelixConstants;
+import com.baozun.nebula.web.MemberDetails;
+import com.baozun.nebula.web.NeedLogin;
+import com.baozun.nebula.web.constants.SessionKeyConstants;
+
+/**
+ * 新版的用户信息拦截器，用于拦截需要登录的请求，并跳转
+ * 对于NeedLogin(guest=true)的情况，当会话中 GUEST_ENTER_ONCE 被设置为true时允许透传过去，此时会话memberDetails为空
+ * 对于NeedLogin的情况，即使会话中 GUEST_ENTER_ONCE 被设置为true，也不会透传过去
+ * 游客标识 GUEST_ENTER_ONCE 在校验后会被删除
+ * 
+ * SecureSessionSignatureHandler 是用于混合模式下保证会员会话安全的处理器
+ * LoginForwardHandler 是用于登录后地址跳转控制的控制器
+ * 这两个都可以被重载后注入
+ * 
+ * @author Benjamin.Liu
+ *
+ */
+public class MemberDetailsInterceptor extends HandlerInterceptorAdapter implements ServletContextAware{
+
+	private static final Logger LOG = LoggerFactory.getLogger(MemberDetailsInterceptor.class);
+	
+	public static final String GUEST_ENTER_ONCE = "login.guestAllowed";
+	
+	@Autowired
+	@Qualifier("secureSessionSignatureHandler")
+	private SecureSessionSignatureHandler secureSessionSignatureHandler;
+	
+	@Autowired
+	@Qualifier("loginForwardHandler")
+	private LoginForwardHandler loginForwardHandler;
+	
+	private ServletContext servletContext;
+	
+	private int ajaxErrorCode = 900;
+	
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+	
+	@Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+       
+        if (handler instanceof HandlerMethod) {
+
+            HandlerMethod method = (HandlerMethod) handler;         
+            NeedLogin needLogin = method.getMethodAnnotation(NeedLogin.class);
+            if (needLogin == null) {
+                needLogin = method.getMethod().getDeclaringClass().getAnnotation(NeedLogin.class);
+            }
+            if (needLogin != null){
+            	LOG.debug("Request need Login: {}", request.getRequestURI());
+                return doValidateNeedLogin(request, response, needLogin);
+            }else {
+            	LOG.debug("Request don't need Login: {}", request.getRequestURI());
+            }
+           
+        }
+
+        return true;
+    }
+	
+	private String getReturnURL(HttpServletRequest request, NeedLogin needLogin){
+		if(StringUtils.isEmpty(needLogin.returnUrl())){
+			String url = new UrlPathHelper().getPathWithinApplication(request);
+			String queryParam = request.getQueryString();
+			if (StringUtils.isNotBlank(queryParam)) {
+				url = url + "?" + queryParam;
+			}
+			return url;
+		}else{
+			LOG.debug("Return URL is found in NeedLogin");
+			return needLogin.returnUrl();
+		}
+	}
+
+	protected boolean doValidateNeedLogin(HttpServletRequest request, HttpServletResponse response, 
+			NeedLogin needLogin) throws IOException {
+		//获取Session中的MemberDetails，该信息是在登录成功后放入会话的，具体可以看
+		//NebulaLoginController中的onAuthenticationSuccess方法
+		MemberDetails memberDetails = (MemberDetails) request.getSession().getAttribute(SessionKeyConstants.MEMBER_CONTEXT);
+    	
+		Boolean guestAllowed = (Boolean) request.getSession().getAttribute(GUEST_ENTER_ONCE);
+		if(memberDetails == null){
+			if(Boolean.TRUE.equals(guestAllowed) && needLogin.guest()){
+				LOG.debug("Guest action for next step: {}", request.getRequestURI());
+				//删除游客标志
+				request.getSession().removeAttribute(GUEST_ENTER_ONCE);
+				return true;
+			}else{
+				LOG.info("No Login information found, need re-login");
+				String returnURL = getReturnURL(request, needLogin);
+				LOG.info("Current Request URL [{}] will be used for return URL after success login", returnURL);
+				
+				LOG.debug("Enter Login Page");
+				interceptLogin(request, response, returnURL);
+				return false;
+			}			
+		}else{
+			//会员已经登录，需要检查当前会话是否安全
+			if(LOG.isDebugEnabled()){
+				LOG.debug("Active Member {} is found for next action.", memberDetails.getLoginName());
+				LOG.debug("Session need be validated before next action");
+			}
+			if(secureSessionSignatureHandler.CheckSignature(request, response)){
+				LOG.debug("Session is safe");
+				return true;
+			}else{
+				LOG.info("Session Signature check is not passed.");
+				secureSessionSignatureHandler.deleteSignature(response);
+				onMemberCheckFailure(request, response);
+				return false;
+			}
+		}
+	}
+	
+	/**
+	 * 跳转到登录的方法，可重载，默认是到达登录页面，并记录登录成功后的跳转地址
+	 * @param request
+	 * @param response
+	 * @param returnURL
+	 * @throws IOException
+	 */
+	protected void interceptLogin(HttpServletRequest request, HttpServletResponse response, String returnURL) throws IOException {
+		if (request.getHeader("X-Requested-With") == null) {    
+			//注意在LoginController中需要使用loginForwardHandler来重定向
+			loginForwardHandler.setForwardURL(request, returnURL);
+            response.sendRedirect(request.getContextPath() + HelixConfig.getInstance().get(HelixConstants.SITE_LOGIN_URL));
+        } else {               	
+            response.sendError(ajaxErrorCode);
+        }
+	}
+	
+	/**
+	 * 用户会话检查失败后的方法，可重载，默认是去往会话检查失败的URL
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	protected void onMemberCheckFailure(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		if (request.getHeader("X-Requested-With") == null) {               	
+            response.sendRedirect(request.getContextPath() + HelixConfig.getInstance().get(HelixConstants.SECURITY_SESSION_CHECK_FAILURE_URL));
+        } else {               	
+            response.sendError(ajaxErrorCode);
+        }
+	}
+
+	public int getAjaxErrorCode() {
+		return ajaxErrorCode;
+	}
+
+	public void setAjaxErrorCode(int ajaxErrorCode) {
+		this.ajaxErrorCode = ajaxErrorCode;
+	}
+}
