89379d6cd36c7ed36efdb3ce4a144431b7d73441
jiulin.he <jiulin.he@baozun.cn>
2016-03-22 17:41:56 +0800

    test

--- nebula-helix/src/main/java/com/baozun/nebula/web/NoClientCache.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/NoClientCache.java
@@ -4,16 +4,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
  * 表示在会输出到client时，会在response header中加上nocache的标识
  * 用于穿透cdn,强制要求浏览器不缓存
+ * 
  * @author Justin Hu
- *
+ * @deprecated 请使用 {@link com.baozun.nebula.web.interceptor.clientcache.ClientCache}替代
  */
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
+@Deprecated
 public @interface NoClientCache{
-
-	boolean value() default true;
+ 
+    boolean value() default true;
 
 }
