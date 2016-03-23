8612c4da0c47d3e5573371a8494f9b3670f09ab5
Administrator <Administrator@10.8.13.34>
2016-03-23 18:13:21 +0800

    3.23

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/member/form/ForgetPasswordForm.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/member/form/ForgetPasswordForm.java
@@ -2,40 +2,50 @@ package com.baozun.nebula.web.controller.member.form;
 
 import com.baozun.nebula.web.controller.BaseForm;
 
-public class ForgetPasswordForm extends BaseForm{
+public class ForgetPasswordForm extends BaseForm {
 
-	private static final long serialVersionUID = -2376678079825089890L;
-	
-	/* 验证方式  可选值 1.mobile  2.email*/
-	private Integer type;
+    private static final long serialVersionUID = -2376678079825089890L;
 
-	private String mobile;
-	
-	private String email;
+    /* 验证方式 可选值 1.mobile 2.email */
+    private Integer type;
 
-	public Integer getType() {
-		return type;
-	}
+    private String mobile;
 
-	public void setType(Integer type) {
-		this.type = type;
-	}
+    private String email;
 
-	public String getMobile() {
-		return mobile;
-	}
+    /* 验证码 */
+    private String SecurityCode;
 
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
+    public String getSecurityCode() {
+	return SecurityCode;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setSecurityCode(String securityCode) {
+	SecurityCode = securityCode;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
+    public Integer getType() {
+	return type;
+    }
+
+    public void setType(Integer type) {
+	this.type = type;
+    }
+
+    public String getMobile() {
+	return mobile;
+    }
+
+    public void setMobile(String mobile) {
+	this.mobile = mobile;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
 }
