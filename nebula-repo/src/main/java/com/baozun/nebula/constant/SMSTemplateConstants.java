c2298c0c1f0c4af47048daacec99bac854e8fad4
yi.huang <yi.huang@baozun.com>
2016-03-30 16:17:06 +0800

    add  SMSManager

---
+++ nebula-repo/src/main/java/com/baozun/nebula/constant/SMSTemplateConstants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.baozun.nebula.constant;
+
+/**
+ * 短信模板code 常量
+ * 
+ * @author Viktor Huang
+ * @date 2016年3月30日 上午11:07:10
+ */
+public class SMSTemplateConstants{
+
+	/** 注册发送验证码短信模板 */
+	public static final String	SMS_REGISTER_CAPTCHA	= "register_captcha";
+}
