d7c8bac9253f4ca29754476edce0749cdaf3a77d
shouqun.li <shouqun.li@baozun.com>
2016-03-30 18:29:59 +0800

    smsCommandTest修改

--- nebula-helix/src/test/java/com/baozun/nebula/manager/message/SmsManagerTest.java
+++ nebula-helix/src/test/java/com/baozun/nebula/manager/message/SmsManagerTest.java
@@ -2,15 +2,24 @@ package com.baozun.nebula.manager.message;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.baozun.nebula.command.SMSCommand;
-import com.baozun.nebula.constants.MessageConstants;
+import com.baozun.nebula.dao.system.SMSTemplateDao;
+import com.baozun.nebula.manager.VelocityManager;
+import com.baozun.nebula.model.system.SMSTemplate;
 import com.baozun.nebula.sdk.manager.SdkSMSManager;
+import com.baozun.nebula.sdk.manager.SdkSMSManager.SendResult;
 import com.baozun.nebula.sdk.manager.impl.SdkSMSManagerImpl;
 import com.baozun.nebula.web.controller.BaseControllerTest;
-import com.feilong.core.util.RandomUtil;
 
 /**
  * 短信验证码manager的Test
@@ -20,24 +29,41 @@ import com.feilong.core.util.RandomUtil;
 public class SmsManagerTest extends BaseControllerTest{
 	
 	private SdkSMSManager smsManager;
+	private List<SMSTemplate> smsTemplate;
+	private String content;
+	private SMSTemplateDao smsTemplateDao;
+	private VelocityManager velocityManager;
+	private SMSCommand smsCommand;
+	private SMSTemplate template;
+	private Map<String, Object> paramMap;
 	@Before
 	public void setUp(){
+		paramMap = new HashMap<String, Object>();
+	    smsTemplate = new ArrayList<SMSTemplate>();
+		template = new SMSTemplate();
+		template.setBody("the content");
+		template.setCode("12");
+		smsTemplate.add(template);
+		content = "the message content";
+		smsCommand = new SMSCommand();
+		smsCommand.setMobile("18271265526");
+		smsCommand.setTemplateCode("12");
+		paramMap.put("code", smsCommand.getTemplateCode());
+		smsTemplateDao = control.createMock("smsTemplateDao", SMSTemplateDao.class);
+		velocityManager = control.createMock("velocityManager", VelocityManager.class);
 		smsManager = new SdkSMSManagerImpl();
+		ReflectionTestUtils.setField(smsManager, "smsTemplateDao", smsTemplateDao);
+		ReflectionTestUtils.setField(smsManager, "velocityManager", velocityManager);
+		
 	}
 	
 	@Test
 	public void smsSend() throws Exception{
-		SMSCommand messageCommand = new SMSCommand();
-		messageCommand.setMobile("18271265526");
-		/*生成验证码*/
-		String code = RandomUtil.createRandomFromString(MessageConstants.SECURITY_CODE_ORIGINAL_STRING, MessageConstants.SECURITY_CODE_LENGTH);
-		System.out.println(code);
-		//ProfileConfigUtil.setMode("dev");
-		//这个sign是必须的（可能是短信供应商那边有设置），每个商城的sign不一样。若没有sign，程序这边显示的短信发送成功，用户实际上是没有收到的。
-		//String sign = ProfileConfigUtil.findPro("config/sms.properties").getProperty("sms.sign");
-		//messageCommand.setContent(code + "测试使用");
+		EasyMock.expect(smsTemplateDao.findEffectSMSTemplateListByQueryMap(paramMap)).andReturn(smsTemplate);
+		EasyMock.expect(velocityManager.parseVMContent(template.getBody(), smsCommand.getVars())).andReturn(content);
+		EasyMock.expectLastCall();
 		control.replay();
-		//assertEquals(true, smsManager.sendMessage(messageCommand));
+		assertEquals(SendResult.SUCESS, smsManager.send(smsCommand));
 		control.verify();
 	}
 }
