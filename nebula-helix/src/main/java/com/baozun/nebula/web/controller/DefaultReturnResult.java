513612c7e8ee61e17f1e13d0986cc18f29860dc2
Benjamin.Liu <benjamin.liu@baozun.com>
2016-04-01 17:43:35 +0800

    重构DefaultReturnResult，里面的SUCCESS将不可修改 增加test-out 目录到忽略目录中

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/DefaultReturnResult.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/DefaultReturnResult.java
@@ -35,7 +35,12 @@ public class DefaultReturnResult extends AbstractReturnResult {
 	/**
 	 * 默认返回成功类
 	 */
-	public static final DefaultReturnResult SUCCESS = new DefaultReturnResult();
+	public static final DefaultReturnResult SUCCESS = new ImmutableReturnResult(true);
+	
+	/**
+	 * 默认返回失败类
+	 */
+	public static final DefaultReturnResult FAILURE = new ImmutableReturnResult(false);
 
 	/**
 	 * 额外的返回信息，当前端需要返回多个信息时使用
@@ -48,5 +53,41 @@ public class DefaultReturnResult extends AbstractReturnResult {
 
 	public void setExtraResultMessages(List<DefaultResultMessage> extraResultMessages) {
 		this.extraResultMessages = extraResultMessages;
+	}
+	
+	private static class ImmutableReturnResult extends DefaultReturnResult {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3687715075882594379L;
+
+		public ImmutableReturnResult(boolean result){
+			super.setResult(result);
+		}
+
+		@Override
+		public void setExtraResultMessages(List<DefaultResultMessage> extraResultMessages) {
+			throw new RuntimeException("Cannot change Immutable Object property.");
+		}
+
+		@Override
+		public void setResult(boolean result) {
+			throw new RuntimeException("Cannot change Immutable Object property.");
+		}
+
+		@Override
+		public void setStatusCode(String statusCode) {
+			throw new RuntimeException("Cannot change Immutable Object property.");
+		}
+
+		@Override
+		public void setResultMessage(DefaultResultMessage resultMessage) {
+			throw new RuntimeException("Cannot change Immutable Object property.");
+		}
+
+		@Override
+		public void setReturnObject(Object returnObject) {
+			throw new RuntimeException("Cannot change Immutable Object property.");
+		}
 	}
 }
