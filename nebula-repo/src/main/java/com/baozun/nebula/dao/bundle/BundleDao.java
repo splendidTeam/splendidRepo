b960b1d8ac0d7b5b378c6c00618bb046932c1e80
yue.cheng <yue.cheng@baozun.com>
2016-04-19 17:03:31 +0800

    新建Bundle相关dao

---
+++ nebula-repo/src/main/java/com/baozun/nebula/dao/bundle/BundleDao.java
@@ -0,0 +1,46 @@
+/**
+ 
+* Copyright (c) 2014 Baozun All Rights Reserved.
+ 
+*
+ 
+* This software is the confidential and proprietary information of Baozun.
+ 
+* You shall not disclose such Confidential Information and shall use it only in
+ 
+* accordance with the terms of the license agreement you entered into
+ 
+* with Baozun.
+ 
+*
+ 
+* BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ 
+* SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ 
+* IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ 
+* PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ 
+* SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ 
+* THIS SOFTWARE OR ITS DERIVATIVES.
+ 
+*
+ 
+*/
+package com.baozun.nebula.dao.bundle;
+
+import com.baozun.nebula.model.bundle.Bundle;
+
+import loxia.dao.GenericEntityDao;
+
+/**
+ * Bundle Dao
+ * 
+ * @author yue.ch
+ *
+ */
+public interface BundleDao extends GenericEntityDao<Bundle, Long> {
+
+}
