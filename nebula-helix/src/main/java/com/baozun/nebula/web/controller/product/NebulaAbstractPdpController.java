247cd352e3ae58d41bacbe8e0e82c6e67f6aa999
D.C <dianchao.song@baozun.cn>
2016-04-20 18:26:48 +0800

    解决出错

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/product/NebulaAbstractPdpController.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/product/NebulaAbstractPdpController.java
@@ -37,10 +37,7 @@ import com.baozun.nebula.sdk.command.CurmbCommand;
 import com.baozun.nebula.sdk.manager.SdkItemManager;
 import com.baozun.nebula.web.controller.PageForm;
 import com.baozun.nebula.web.controller.product.converter.BreadcrumbsViewCommandConverter;
-<<<<<<< HEAD
 import com.baozun.nebula.web.controller.product.resolver.ItemPropertyViewCommandResolver;
-=======
->>>>>>> branch 'master' of http://git.baozun.cn/nebula/nebula.git
 import com.baozun.nebula.web.controller.product.converter.ImageViewCommandConverter;
 import com.baozun.nebula.web.controller.product.viewcommand.BreadcrumbsViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ImageViewCommand;
@@ -119,14 +116,6 @@ public abstract class NebulaAbstractPdpController extends NebulaBasePdpControlle
 	@Autowired
 	ImageViewCommandConverter                                       imageViewCommandConverter;
 	
-<<<<<<< HEAD
-	
-	
-
-	
-=======
->>>>>>> branch 'master' of http://git.baozun.cn/nebula/nebula.git
-
 	
 	/**
 	 * <p>构造商品的属性信息，包括销售属性和非销售属性</p>
