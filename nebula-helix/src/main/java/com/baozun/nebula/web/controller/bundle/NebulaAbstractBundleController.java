fe0129bdd4ad1efa12bfae32fdbc4cbadfb60b5a
yue.cheng <yue.cheng@baozun.com>
2016-04-20 14:06:24 +0800

    add bundle controller

---
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/bundle/NebulaAbstractBundleController.java
@@ -0,0 +1,50 @@
+/**
+ 
+* Copyright (c) 2014 Baozun All Rights Reserved.
+ 
+*
+ 
+* This software is the confidential and proprietary information of Baozun.
+ 
+* You shall not disclose such Confidential Information and shall use it only in
+ 
+* accordance with the terms of the license agreement you entered into
+ 
+* with Baozun.
+ 
+*
+ 
+* BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ 
+* SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ 
+* IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ 
+* PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ 
+* SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ 
+* THIS SOFTWARE OR ITS DERIVATIVES.
+ 
+*
+ 
+*/
+package com.baozun.nebula.web.controller.bundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baozun.nebula.web.controller.BaseController;
+
+/**
+ * 捆绑类商品 controller
+ * 
+ * @author yue.ch
+ *
+ */
+public abstract class NebulaAbstractBundleController extends BaseController {
+	
+	private static final Logger	LOG									= LoggerFactory.getLogger(NebulaAbstractBundleController.class);
+
+	 
+}
---
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/bundle/viewcommand/BundleElementViewCommand.java
@@ -0,0 +1,44 @@
+/**
+ 
+* Copyright (c) 2014 Baozun All Rights Reserved.
+ 
+*
+ 
+* This software is the confidential and proprietary information of Baozun.
+ 
+* You shall not disclose such Confidential Information and shall use it only in
+ 
+* accordance with the terms of the license agreement you entered into
+ 
+* with Baozun.
+ 
+*
+ 
+* BAOZUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ 
+* SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ 
+* IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ 
+* PURPOSE, OR NON-INFRINGEMENT. BAOZUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ 
+* SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ 
+* THIS SOFTWARE OR ITS DERIVATIVES.
+ 
+*
+ 
+*/
+package com.baozun.nebula.web.controller.bundle.viewcommand;
+
+import com.baozun.nebula.web.controller.BaseViewCommand;
+
+/**
+ * @author yue.ch
+ *
+ */
+public class BundleElementViewCommand extends BaseViewCommand {
+
+	private static final long serialVersionUID = 2340192122967455038L;
+
+}
