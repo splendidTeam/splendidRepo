f6cb26ef51f203ee6e64ada719c2c0ecd04f21f8
yimin.qiao <yimin.qiao@baozun.com>
2016-04-20 16:47:03 +0800

    提取pdp公共方法供bundle使用

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/product/NebulaAbstractPdpController.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/product/NebulaAbstractPdpController.java
@@ -17,7 +17,6 @@
  */
 package com.baozun.nebula.web.controller.product;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,32 +28,23 @@ import loxia.dao.Pagination;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.baozun.nebula.manager.product.ItemDetailManager;
 import com.baozun.nebula.model.product.ItemImage;
 import com.baozun.nebula.sdk.command.CurmbCommand;
-import com.baozun.nebula.sdk.command.ItemBaseCommand;
-import com.baozun.nebula.sdk.command.SkuCommand;
 import com.baozun.nebula.sdk.manager.SdkItemManager;
-import com.baozun.nebula.web.controller.BaseController;
 import com.baozun.nebula.web.controller.PageForm;
 import com.baozun.nebula.web.controller.product.converter.BreadcrumbsViewCommandConverter;
-import com.baozun.nebula.web.controller.product.converter.SkuViewCommandConverter;
 import com.baozun.nebula.web.controller.product.viewcommand.BreadcrumbsViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ImageViewCommand;
-import com.baozun.nebula.web.controller.product.viewcommand.InventoryViewCommand;
-import com.baozun.nebula.web.controller.product.viewcommand.ItemBaseInfoViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemCategoryViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemExtraViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemImageViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemPropertyViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemRecommendViewCommand;
 import com.baozun.nebula.web.controller.product.viewcommand.ItemReviewViewCommand;
-import com.baozun.nebula.web.controller.product.viewcommand.PriceViewCommand;
-import com.baozun.nebula.web.controller.product.viewcommand.SkuViewCommand;
 import com.feilong.core.Validator;
 
 
@@ -69,7 +59,7 @@ import com.feilong.core.Validator;
  * @version 1.0
  * @time 2016年4月14日  上午10:15:30
  */
-public abstract class NebulaAbstractPdpController extends BaseController {
+public abstract class NebulaAbstractPdpController extends NebulaBasePdpController {
 
 	/**
 	 * log定义
@@ -118,26 +108,6 @@ public abstract class NebulaAbstractPdpController extends BaseController {
 	@Qualifier("breadcrumbsViewCommandConverter")
 	private BreadcrumbsViewCommandConverter							breadcrumbsViewCommandConverter;
 	
-	@Autowired
-	@Qualifier("skuViewCommandConverter")
-	private SkuViewCommandConverter									skuViewCommandConverter;
-	
-
-	
-	/**
-	 * 构造商品基本信息
-	 * 这个方法暂时不用考虑pdp显示模式的问题，因为基本信息对于几种模式取法是一样的
-	 * @param itemId
-	 * @return
-	 */
-	protected ItemBaseInfoViewCommand buildProductBaseInfoViewCommand(Long itemId) {
-		ItemBaseInfoViewCommand itemBaseInfoViewCommand = new ItemBaseInfoViewCommand();
-		ItemBaseCommand itemBaseCommand = sdkItemManager.findItemBaseInfoLang(itemId);
-		if(itemBaseCommand!=null){
-			BeanUtils.copyProperties(itemBaseCommand, itemBaseInfoViewCommand);
-		}
-		return itemBaseInfoViewCommand;
-	}
 	
 	/**
 	 * 构造商品的属性信息，包括销售属性和非销售属性
@@ -152,89 +122,7 @@ public abstract class NebulaAbstractPdpController extends BaseController {
 		return new ItemPropertyViewCommand();
 	}
 	
-	/**
-	 * 构造sku信息
-	 * @param itemId
-	 * @return
-	 */
-	protected List<SkuViewCommand> buildSkuViewCommand(Long itemId) {
-		assert itemId != null : "Please Check itemId!";
-		List<SkuCommand> commands =itemDetailManager.findEffectiveSkuInvByItemId(itemId);
-		if(Validator.isNotNullOrEmpty(commands)){
-			List<SkuViewCommand> skuViewCommands =skuViewCommandConverter.convert(commands);
-			for (SkuViewCommand skuViewCommand : skuViewCommands) {
-				skuViewCommand.setItemId(itemId);
-			}
-			return skuViewCommands;
-		}
-		return null;
-	}
 	
-	/**
-	 * 构造库存信息
-	 * @param itemId
-	 * @return
-	 */
-	protected List<InventoryViewCommand> buildInventoryViewCommand(Long itemId) {
-		List<InventoryViewCommand> inventoryViewCommands = new ArrayList<InventoryViewCommand>();
-		List<SkuCommand> skuCommands = sdkItemManager.findInventoryByItemId(itemId);
-		if(Validator.isNotNullOrEmpty(skuCommands)){
-			for(SkuCommand skuCommand:skuCommands){
-				InventoryViewCommand inventoryViewCommand = new InventoryViewCommand();
-				BeanUtils.copyProperties(skuCommand, inventoryViewCommand);
-				inventoryViewCommand.setSkuId(skuCommand.getId());
-				inventoryViewCommands.add(inventoryViewCommand);
-			}
-		}
-		return inventoryViewCommands;
-	}
-	
-	/**
-	 * 构造价格信息
-	 * @return
-	 */
-	protected PriceViewCommand buildPriceViewCommand(ItemBaseInfoViewCommand baseInfoViewCommand,
-			List<SkuViewCommand> skuViewCommands) {
-		assert baseInfoViewCommand != null && Validator.isNullOrEmpty(skuViewCommands)
-				: "Please Check baseInfo and skuInfos!";
-		PriceViewCommand priceViewCommand =new PriceViewCommand();
-		priceViewCommand.setItemListPrice(baseInfoViewCommand.getListPrice());
-		priceViewCommand.setItemSalesPrice(baseInfoViewCommand.getSalePrice());
-		
-		BigDecimal skuMinListPrice=BigDecimal.ZERO;
-		BigDecimal skuMaxListPrice=BigDecimal.ZERO;
-		BigDecimal skuMinSalesPrice=BigDecimal.ZERO;
-		BigDecimal skuMaxSalesPrice=BigDecimal.ZERO;
-		int i =0;
-		for (SkuViewCommand skuViewCommand : skuViewCommands) {
-			if(i == 0){
-				skuMinListPrice =skuViewCommand.getListPrice();
-				skuMaxListPrice =skuViewCommand.getListPrice();
-				skuMinSalesPrice =skuViewCommand.getSalePrice();
-				skuMaxSalesPrice =skuViewCommand.getSalePrice();
-			}else{
-				//>
-				if(skuMinListPrice.compareTo(skuViewCommand.getListPrice()) == 1){
-					skuMinListPrice =skuViewCommand.getListPrice();
-				}
-				//<
-				if(skuMaxListPrice.compareTo(skuViewCommand.getListPrice()) == -1){
-					skuMaxListPrice =skuViewCommand.getListPrice();
-				}
-				//>
-				if(skuMinSalesPrice.compareTo(skuViewCommand.getSalePrice()) == 1){
-					skuMinSalesPrice =skuViewCommand.getSalePrice();
-				}
-				//<
-				if(skuMaxSalesPrice.compareTo(skuViewCommand.getSalePrice()) == -1){
-					skuMaxSalesPrice =skuViewCommand.getSalePrice();
-				}
-			}
-			i++;
-		}
-		
-		return new PriceViewCommand();
-	}
 	
 	/**
 	 * 构造商品的图片
