a27adefe78c9ff779cf9cf36142e22bbc64dfb2c
xin.jin <xin.jin@baozun.com>
2016-05-04 18:37:59 +0800

    update javadoc

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/shoppingcart/NebulaShoppingCartController.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/shoppingcart/NebulaShoppingCartController.java
@@ -45,6 +45,15 @@ import com.baozun.nebula.web.controller.shoppingcart.resolver.ShoppingcartResult
 /**
  * 购物车控制器.
  * 
+ * <h3>定义:</h3>
+ * 
+ * <blockquote>
+ * <p>
+ * 网上商店所说的购物车是对现实的购物车而喻，买家可以像在超市里购物一样，随意添加、删除商品，选购完毕后，统一下单<br>
+ * 网上商店的购物车要能过跟踪顾客所选的的商品，记录下所选商品，还要能随时更新，可以支付购买，能给顾客提供很大的方便
+ * </p>
+ * </blockquote>
+ * 
  * <p>
  * 主要由以下方法组成:
  * </p>
@@ -223,7 +232,7 @@ public class NebulaShoppingCartController extends BaseController{
 
         return toNebulaReturnResult(shoppingcartResult);
     }
-    
+
     //**********************************updateShoppingCartCount************************************************
     /**
      * 删除购物车行.
@@ -419,22 +428,23 @@ public class NebulaShoppingCartController extends BaseController{
     private ShoppingcartResolver detectShoppingcartResolver(MemberDetails memberDetails){
         return null == memberDetails ? guestShoppingcartResolver : memberShoppingcartResolver;
     }
-    
+
     /**
      * 返回结果填充
+     * 
      * @param shoppingcartResult
      * @return
      */
-    private NebulaReturnResult toNebulaReturnResult(ShoppingcartResult shoppingcartResult) {
+    private NebulaReturnResult toNebulaReturnResult(ShoppingcartResult shoppingcartResult){
         if (ShoppingcartResult.SUCCESS != shoppingcartResult){
-        	DefaultReturnResult  result = DefaultReturnResult.FAILURE;
-        	
-        	String messageStr = getMessage(shoppingcartResult.toString());
-        	
+            DefaultReturnResult result = DefaultReturnResult.FAILURE;
+
+            String messageStr = getMessage(shoppingcartResult.toString());
+
             DefaultResultMessage message = new DefaultResultMessage();
-			message.setMessage(messageStr);
+            message.setMessage(messageStr);
             result.setResultMessage(message);
-            
+
             LOGGER.error(messageStr);
             return result;
         }
