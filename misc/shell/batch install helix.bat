63c783c8cb949af1eac1d29f3b4558294e7ee419
xin.jin <xin.jin@baozun.com>
2016-05-07 20:00:51 +0800

    update bat

--- misc/shell/batch install helix.bat
+++ misc/shell/batch install helix.bat
@@ -1,6 +1,12 @@
 ::	
 ::		一键式 clean install nebula-helix
 ::
+::
+::  现在helix是nebula的子项目,并且依赖于nebula-repo,repo依赖于nebula-utilities, 
+::	如果nebula-utilities或者nebula-repo,有任何的代码调整,通常我们需要先install nebula-utilities或者nebula-repo
+::	此时比较繁琐,因此这里提供一键式 clean install nebula-helix的功能
+::
+::
 ::step1 基于nebula parent目录 svn更新
 ::
 ::step2 双击执行这个脚本,会自动 install parent 项目, 
