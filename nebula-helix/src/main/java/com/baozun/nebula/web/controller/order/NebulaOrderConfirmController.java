0cc3666ed155cc9379b5b8d4d623c735fdd24c55
dahui.huang <dahui.huang@baozun.cn>
2016-05-11 15:15:54 +0800

    NebulaOrderConfirmController的detectShoppingcartResolver 修饰符调整

--- nebula-helix/src/main/java/com/baozun/nebula/web/controller/order/NebulaOrderConfirmController.java
+++ nebula-helix/src/main/java/com/baozun/nebula/web/controller/order/NebulaOrderConfirmController.java
@@ -295,7 +295,7 @@ public class NebulaOrderConfirmController extends BaseController {
 	 *            the member details
 	 * @return the shoppingcart resolver
 	 */
-	private ShoppingcartResolver detectShoppingcartResolver(MemberDetails memberDetails) {
+	protected ShoppingcartResolver detectShoppingcartResolver(MemberDetails memberDetails) {
 		return null == memberDetails ? guestShoppingcartResolver : memberShoppingcartResolver;
 	}
 
