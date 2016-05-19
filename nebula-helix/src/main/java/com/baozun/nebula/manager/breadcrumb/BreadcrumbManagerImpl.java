bca493f17de3c0713fb80a7a91e554e24088f95b
dongliang.ma <dongliang.ma@baozun.cn>
2016-05-19 10:26:29 +0800

    修复facetFilterHelper无法实例的错误

--- nebula-helix/src/main/java/com/baozun/nebula/manager/breadcrumb/BreadcrumbManagerImpl.java
+++ nebula-helix/src/main/java/com/baozun/nebula/manager/breadcrumb/BreadcrumbManagerImpl.java
@@ -32,6 +32,7 @@ import loxia.dao.Sort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
@@ -116,6 +117,7 @@ public class BreadcrumbManagerImpl implements BreadcrumbManager {
 	private SdkItemCollectionManager								sdkItemCollectionManager;
 	
 	@Autowired
+	@Qualifier("facetFilterHelper")
 	private FacetFilterHelper										facetFilterHelper;
 	
 	@Autowired
