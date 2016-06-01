6e63d44598b13ba484028666efa4b4ab3840e276
yue.cheng <yue.cheng@baozun.com>
2016-06-01 15:37:58 +0800

    添加页面元素点击事件

--- nebula-protostar/src/main/webapp/scripts/product/item/add-item-bundle.js
+++ nebula-protostar/src/main/webapp/scripts/product/item/add-item-bundle.js
@@ -1,3 +1,15 @@
+$j.extend(loxia.regional['zh-CN'],{
+	"BUNDLE_DIALOG_TITLE_MAIN":"选择主卖品",
+	"BUNDLE_DIALOG_TITLE_ELEMENT":"选择成员",
+	"NO_DATA":"无数据",
+	"TABLE_TITLE_ITEM":"商品",
+	"TABLE_TITLE_ITEM_CATEGROY":"商品分类",
+	"TABLE_TITLE_ITEM_TYPE":"商品种类",
+	"TABLE_TITLE_ITEM_PRICE":"商品价格",
+	"TABLE_TITLE_ITEM_STATUS":"商品状态",
+	"TABLE_TITLE_STYLE_CODE":"款号"
+});
+
 $j(document).ready(function(){
 	$j(':radio[name=type]').bind('change', function(){
 		var currVal = $j(this).val();
@@ -10,6 +22,23 @@ $j(document).ready(function(){
 			$j('#selectProList_style').show();
 		}
 	});
+	
+	// 点击添加主卖品
+	$j("#set_main_element").on("click",function(){
+		$j('.select-pro-layer').dialogff({type:'open',close:'in',width:'900px', height:'500px'});
+		$j('#bundle_dialog_title').html(nps.i18n("BUNDLE_DIALOG_TITLE_MAIN"));
+	});	
+	
+	// 点击添加捆绑成员
+	$j("#add_bundle_element").on("click",function(){
+		$j('.select-pro-layer').dialogff({type:'open',close:'in',width:'900px', height:'500px'});	
+		$j('#bundle_dialog_title').html(nps.i18n("BUNDLE_DIALOG_TITLE_ELEMENT"));
+	});	
+	
+	// 点击删除bundle成员
+	$j(".dialog_close").on("click", function(){
+		
+	});
 });
 
 function findProduct(){
@@ -19,24 +48,30 @@ function findProduct(){
 		nodatamessage : '<span>' + nps.i18n("NO_DATA") + '</span>',
 		form : "mainItemDialogSearchForm",
 		cols : [ {
+			name : "item.id",
 			label : "",
 			width : "5%",
-			template : "checkboxTemplate",
+			template : "radioTemplate",
 		}, {
-			name : "code",
-			label : "商品编码",
-			width : "10%",
-			sort: ["tpi.code asc","tpi.code desc"]
+			name : "item.name",
+			label : nps.i18n("TABLE_TITLE_ITEM"),
+			width : "30%"
 		}, {
-			name : "title",
-			label : "商品名称",
-			width : "20%",
-			sort: ["tpii.title asc","tpii.title desc"]
+			name : "item.categorys",
+			label : nps.i18n("TABLE_TITLE_ITEM_CATEGROY"),
+			width : "30%"
 		}, {
-			name : "操作",
-			label : "操作",
-			width : "5%",
-			template : "operateTemplate",
+			name : "item.type",
+			label : nps.i18n("TABLE_TITLE_ITEM_TYPE"),
+			width : "10%"
+		}, {
+			name : "item.salesPrice",
+			label : nps.i18n("TABLE_TITLE_ITEM_PRICE"),
+			width : "10%"
+		}, {
+			name : "item.status",
+			label : nps.i18n("TABLE_TITLE_ITEM_STATUS"),
+			width : "10%"
 		}],
 		dataurl : findItemInfoListJsonUrl
 	});
