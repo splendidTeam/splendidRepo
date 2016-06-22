d1803b73c5166e9e1a318d37d08ef2fb839788cb
dongliang.ma <dongliang.ma@baozun.cn>
2016-06-22 10:40:18 +0800

    修复：PTS敏感信息密文显示

--- nebula-repo/src/main/java/com/baozun/nebula/sdk/manager/order/OrderManagerImpl.java
+++ nebula-repo/src/main/java/com/baozun/nebula/sdk/manager/order/OrderManagerImpl.java
@@ -215,6 +215,12 @@ public class OrderManagerImpl implements OrderManager{
 
         List<Long> idList = new ArrayList<Long>(salesOrderPage.size());
         for (SalesOrderCommand cmd : salesOrderPage){
+        	boolean isDecrypt =Validator.isNotNullOrEmpty(searchParam)&&
+        			Validator.isNotNullOrEmpty(searchParam.get("sdkQueryType"))&&
+        			searchParam.get("sdkQueryType").equals("1");
+        	if(isDecrypt){
+        		decryptSalesOrderCommand(cmd);
+        	}
             idList.add(cmd.getId());
         }
 
@@ -249,7 +255,13 @@ public class OrderManagerImpl implements OrderManager{
         List<SalesOrderCommand> salesOrderCommandList = salesOrderPage.getItems();
         if (null != salesOrderCommandList){
             List<Long> idList = new ArrayList<Long>(salesOrderCommandList.size());
+            boolean isDecrypt =Validator.isNotNullOrEmpty(searchParam)&&
+        			Validator.isNotNullOrEmpty(searchParam.get("sdkQueryType"))&&
+        			searchParam.get("sdkQueryType").equals("1");
             for (SalesOrderCommand cmd : salesOrderCommandList){
+            	if(isDecrypt){
+            		decryptSalesOrderCommand(cmd);
+            	}
                 idList.add(cmd.getId());
             }
 
