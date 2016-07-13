c58449569e5f04793d9b63aa3a00b413b1e0ed14
xingyu.liu <xingyu.liu@baozun.com>
2016-07-13 15:33:46 +0800

    在系统初始化时的listener中，添加收货地址初始化方法

--- nebula-protostar/src/main/java/com/baozun/nebula/web/listener/NebulaSystemInitListener.java
+++ nebula-protostar/src/main/java/com/baozun/nebula/web/listener/NebulaSystemInitListener.java
@@ -8,9 +8,11 @@ import javax.servlet.ServletContextListener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import com.baozun.nebula.command.i18n.LangProperty;
 import com.baozun.nebula.curator.watcher.DefaultNodeDataChangedWatcher;
 import com.baozun.nebula.manager.cms.SdkCmsInitManager;
-import com.baozun.nebula.utilities.common.ProfileConfigUtil;
+import com.baozun.nebula.sdk.manager.SdkI18nLangManager;
+import com.baozun.nebula.utilities.library.address.AddressUtil;
 
 /**
  * 系统初始化时的listener
@@ -24,6 +26,7 @@ public class NebulaSystemInitListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent sce) {
 		initZookeeper(sce);
 		initCms(sce);
+		initUrl(sce);
 		extendProcess(sce);
 	}
 
@@ -61,5 +64,19 @@ public class NebulaSystemInitListener implements ServletContextListener {
 		SdkCmsInitManager sim = (SdkCmsInitManager) applicationContext.getBean("sdkCmsInitManager");
 		sim.megerCacheUrl();
 	}
+	
+	private void initUrl(ServletContextEvent sce){
+		ApplicationContext applicationContext = WebApplicationContextUtils
+				.getRequiredWebApplicationContext(sce.getServletContext());
+
+		SdkI18nLangManager sdkI18nLangManager = (SdkI18nLangManager) applicationContext.getBean("sdkI18nLangManager");
+		// 设置要加载的地址信息
+		AddressUtil.setI18nOffOn(LangProperty.getI18nOnOff());
+		AddressUtil.setLanguageList(sdkI18nLangManager.getAllEnabledI18nKeyList());
+		AddressUtil.init();
+	}
+	
+	
+	
 
 }
