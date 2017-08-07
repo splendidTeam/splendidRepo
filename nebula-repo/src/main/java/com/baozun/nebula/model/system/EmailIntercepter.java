2aa89a659662f8a0b51971ab42a60425ed04ee56
xin.jin <xin.jin@baozun.com>
2017-08-07 21:04:18 +0800

    修改 EmailIntercepter (T_SYS_EMAIL_INTERCEPTER)uniqueContent 字段长度 http://jira.baozun.cn/browse/NB-918

--- nebula-repo/src/main/java/com/baozun/nebula/model/system/EmailIntercepter.java
+++ nebula-repo/src/main/java/com/baozun/nebula/model/system/EmailIntercepter.java
@@ -17,10 +17,10 @@ import org.hibernate.annotations.OptimisticLockType;
 import com.baozun.nebula.command.Command;
 import com.baozun.nebula.model.BaseModel;
 
-
 /**
  * 
  * 邮件拦截（告警邮件发送一定次数 次数自己设置 可停止发送）
+ * 
  * @author lxy
  *
  */
@@ -29,120 +29,109 @@ import com.baozun.nebula.model.BaseModel;
 @org.hibernate.annotations.Entity(optimisticLock = OptimisticLockType.VERSION)
 public class EmailIntercepter extends BaseModel implements Command{
 
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 338588863010924298L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 338588863010924298L;
 
-	/**
-	 * PK
-	 */
-	private Long				id;
-	/**
-	 * 模板编码
-	 */
-	private String				code;
-	/**
-	 * 特征内容
-	 */
-	private String				uniqueContent;
-	/**
-	 * 接收人的email
-	 */
-	private String 				receiverEmail;
-	
-	/**
-	 * 最近发送时间
-	 */
-	private Date				sendTime;
-	
-	/**
-	 * 发送次数
-	 */
-	private Integer 			count;
-	
-	
+    /**
+     * PK
+     */
+    private Long id;
 
-	private Date				version;
+    /**
+     * 模板编码
+     */
+    private String code;
 
+    /**
+     * 特征内容
+     */
+    private String uniqueContent;
 
-	@Id
-	@Column(name = "ID")
-	@SequenceGenerator(name = "SEQ_T_SYS_EMAIL_INTERCEPTER",sequenceName = "S_T_SYS_EMAIL_INTERCEPTER",allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "SEQ_T_SYS_EMAIL_INTERCEPTER")
-	public Long getId() {
-		return id;
-	}
+    /**
+     * 接收人的email
+     */
+    private String receiverEmail;
 
+    /**
+     * 最近发送时间
+     */
+    private Date sendTime;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    /**
+     * 发送次数
+     */
+    private Integer count;
 
-	@Column(name = "CODE")
+    private Date version;
+
+    @Id
+    @Column(name = "ID")
+    @SequenceGenerator(name = "SEQ_T_SYS_EMAIL_INTERCEPTER",sequenceName = "S_T_SYS_EMAIL_INTERCEPTER",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "SEQ_T_SYS_EMAIL_INTERCEPTER")
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    @Column(name = "CODE")
     @Index(name = "IDX_EMAIL_INTERCEPTER_CODE")
-	public String getCode() {
-		return code;
-	}
+    public String getCode(){
+        return code;
+    }
 
+    public void setCode(String code){
+        this.code = code;
+    }
 
-	public void setCode(String code) {
-		this.code = code;
-	}
+    @Column(name = "UNIQUE_CONTENT",length = 4000)
+    public String getUniqueContent(){
+        return uniqueContent;
+    }
 
-	@Column(name = "UNIQUE_CONTENT")
-	public String getUniqueContent() {
-		return uniqueContent;
-	}
+    public void setUniqueContent(String uniqueContent){
+        this.uniqueContent = uniqueContent;
+    }
 
+    @Column(name = "RECEIVER_EMAIL")
+    public String getReceiverEmail(){
+        return receiverEmail;
+    }
 
-	public void setUniqueContent(String uniqueContent) {
-		this.uniqueContent = uniqueContent;
-	}
+    public void setReceiverEmail(String receiverEmail){
+        this.receiverEmail = receiverEmail;
+    }
 
-	@Column(name = "RECEIVER_EMAIL")
-	public String getReceiverEmail() {
-		return receiverEmail;
-	}
+    @Column(name = "SEND_TIME")
+    public Date getSendTime(){
+        return sendTime;
+    }
 
+    public void setSendTime(Date sendTime){
+        this.sendTime = sendTime;
+    }
 
-	public void setReceiverEmail(String receiverEmail) {
-		this.receiverEmail = receiverEmail;
-	}
+    @Column(name = "COUNT")
+    public Integer getCount(){
+        return count;
+    }
 
+    public void setCount(Integer count){
+        this.count = count;
+    }
 
-	@Column(name = "SEND_TIME")
-	public Date getSendTime() {
-		return sendTime;
-	}
+    @Version
+    @Column(name = "VERSION")
+    public Date getVersion(){
+        return version;
+    }
 
+    public void setVersion(Date version){
+        this.version = version;
+    }
 
-	public void setSendTime(Date sendTime) {
-		this.sendTime = sendTime;
-	}
-
-	@Column(name = "COUNT")
-	public Integer getCount() {
-		return count;
-	}
-
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	@Version
-	@Column(name = "VERSION")
-	public Date getVersion() {
-		return version;
-	}
-
-	public void setVersion(Date version) {
-		this.version = version;
-	}
-
-	
-	
-	
 }
